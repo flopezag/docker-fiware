9385ffa255b8d42ee8eb244a8698b403c228e586
Fernando Lopez <fernando.lopez@fiware.org>
2018-05-28 13:56:11 +0200

    Update shell script

--- config-idm.sh
+++ config-idm.sh
@@ -11,27 +11,33 @@ echo
 read -p "Client ID: " clientid
 read -p "Client Secret: " clientsecret
 
+# Add url('', include('social_django.urls', namespace='social')),
+line1="    # 'wirecloud.oauth2provider',"
+line2="    'social_django',"
 
-sed "s/'wirecloud.oauth2provider',/# 'wirecloud.oauth2provider',%    'social_django',/" < ./wirecloud_instance/settings.py | tr "%" "\n" > ./wirecloud_instance/settings1.py
+printf "%s\n%s\n" "$line1" "$line2" > ./wirecloud_instance/a.out
 
-echo "AUTHENTICATION_BACKENDS = ("                              >> ./wirecloud_instance/settings1.py
-echo "    'wirecloud.fiware.social_auth_backend.FIWAREOAuth2'," >> ./wirecloud_instance/settings1.py
-echo "    'wirecloud.fiware.social_auth_backend.FIWAREOAuth2'," >> ./wirecloud_instance/settings1.py
-echo "    'django.contrib.auth.backends.ModelBackend',"         >> ./wirecloud_instance/settings1.py
-echo ")"                                                        >> ./wirecloud_instance/settings1.py
+sed "/'wirecloud.oauth2provider',/ d" ./wirecloud_instance/settings.py > ./wirecloud_instance/settings1.py
+sed "/'wirecloud.fiware',/ r ./wirecloud_instance/a.out" ./wirecloud_instance/settings1.py > ./wirecloud_instance/settings2.py
 
-echo                                                            >> ./wirecloud_instance/settings1.py
+echo "AUTHENTICATION_BACKENDS = ("                              >> ./wirecloud_instance/settings2.py
+echo "    'wirecloud.fiware.social_auth_backend.FIWAREOAuth2'," >> ./wirecloud_instance/settings2.py
+echo "    'django.contrib.auth.backends.ModelBackend',"         >> ./wirecloud_instance/settings2.py
+echo ")"                                                        >> ./wirecloud_instance/settings2.py
+
+echo                                                            >> ./wirecloud_instance/settings2.py
 
 
-echo "FIWARE_IDM_SERVER = 'http://127.0.0.1:3000'"              >> ./wirecloud_instance/settings1.py
-echo                                                            >> ./wirecloud_instance/settings1.py
-echo "SOCIAL_AUTH_FIWARE_KEY = '$clientid'"                     >> ./wirecloud_instance/settings1.py
-echo                                                            >> ./wirecloud_instance/settings1.py
-echo "SOCIAL_AUTH_FIWARE_SECRET = '$clientsecret'"              >> ./wirecloud_instance/settings1.py
+echo "FIWARE_IDM_SERVER = 'http://127.0.0.1:3000'"              >> ./wirecloud_instance/settings2.py
+echo                                                            >> ./wirecloud_instance/settings2.py
+echo "SOCIAL_AUTH_FIWARE_KEY = '$clientid'"                     >> ./wirecloud_instance/settings2.py
+echo                                                            >> ./wirecloud_instance/settings2.py
+echo "SOCIAL_AUTH_FIWARE_SECRET = '$clientsecret'"              >> ./wirecloud_instance/settings2.py
 
 rm ./wirecloud_instance/settings.py
 rm ./wirecloud_instance/settings.pyc
-mv ./wirecloud_instance/settings1.py ./wirecloud_instance/settings.py
+rm ./wirecloud_instance/settings1.py
+mv ./wirecloud_instance/settings2.py ./wirecloud_instance/settings.py
 
 #####################
 # Reconfigure urls.py
